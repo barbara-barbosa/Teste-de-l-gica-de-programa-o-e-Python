856b6fb2c47611a6f4336367cf199da2b284edb0
Bárbara Barbosa <51497274+barbara-barbosa@users.noreply.github.com>
2021-04-28 11:25:49 -0300

    Add files via upload

---
+++ Exercicio_3.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Apr 28 11:10:15 2021
+
+@author: bbarbosa
+
+"""
+print (" 3.Escreva um script em Python que gere a sequência de Fibonacci (em formato de lista) dado um valor n em formato inteiro.")
+x = int( input(" Digite Quantos termos você deseja mostrar na sequência de Fibonacci:")) # Armazena o número digitado inteiro na variavel 
+x1 = 1
+x2 = 0
+x3 = 0
+x4 = []
+
+while x1 <= x: #enquanto for verdadeiro executa (enquanto x1 for menor igual ao valor inserido)
+    x4.append(x1) # Adiciona o valor ao fim da lista enquanto for verdadeiro a condição
+    x3 = x1 + x2 
+    x2 = x1
+    x1 = x3
+
+print(x4) # Escreva o resultado da lista criada na variavel x4
